4de9f33417ebc117f6be499d2a7953b2b0b36b92
zhabaa <vova.dovnar2006@gmail.com>
2024-04-23 21:45:52 +0300

    update some files

--- static/js/main.js
+++ static/js/main.js
@@ -11,37 +11,12 @@ const prevBtnEl = document.getElementById("prev");
 const playvBtnEl = document.getElementById("play");
 const nextvBtnEl = document.getElementById("next");
 
-const songs = [
-  {
-    path: "imgs_audio/1.mp3",
-    displayName: "Курьер",
-    cover: "imgs_audio/img-1.jpg",
-    artist: "Дайте Танк (!)",
-  },
-  {
-    path: "imgs_audio/2.mp3",
-    displayName: "Alone, Pt. II",
-    cover: "imgs_audio/img-2.jpg",
-    artist: "Alan Walker & Ava Max",
-  },
-  {
-    path: "imgs_audio/3.mp3",
-    displayName: "Let Me Love You ft",
-    cover: "imgs_audio/img-3.jpg",
-    artist: "Justin Bieber",
-  },
-  {
-    path: "imgs_audio/4.mp3",
-    displayName: "On The Floor ft",
-    cover: "imgs_audio/img-4.jpg",
-    artist: "Jennifer Lopez",
-  },
-];
+const songs = [];
 
 const music = new Audio();
 let musicIndex = 0;
 let isPlaying = false;
-//================== Play Song  True or False====================
+
 function togglePlay() {
   if (isPlaying) {
     pauseMusic();
@@ -49,21 +24,21 @@ function togglePlay() {
     playMusic();
   }
 }
-//================== Play Music====================
+
 function playMusic() {
   isPlaying = true;
   playvBtnEl.classList.replace("fa-play", "fa-pause");
   playvBtnEl.setAttribute("title", "pause");
   music.play();
 }
-//================== Pause Music====================
+
 function pauseMusic() {
   isPlaying = false;
   playvBtnEl.classList.replace("fa-pause", "fa-play");
   playvBtnEl.setAttribute("pause", "title");
   music.pause();
 }
-//================== Load Songs ====================
+
 function loadMusic(songs) {
   music.src = songs.path;
   musicTitleEl.textContent = songs.displayName;
@@ -71,19 +46,19 @@ function loadMusic(songs) {
   imgCoverEl.src = songs.cover;
   imgEl.src = songs.cover;
 }
-//================== Change Music ====================
+
 function changeMusic(direction) {
   musicIndex = musicIndex + direction + (songs.length % songs.length);
   loadMusic(songs[musicIndex]);
   playMusic();
 }
-//================== Set Progress ====================
+
 function setProgressBar(e) {
   const width = playerProgressEl.clientWidth;
   const xValue = e.offsetX;
   music.currentTime = (xValue / width) * music.duration;
 }
-//================== Set Progress ====================
+
 function updateProgressBar() {
   const { duration, currentTime } = music;
   const ProgressPercent = (currentTime / duration) * 100;
@@ -97,17 +72,17 @@ function updateProgressBar() {
     currentTime % 60,
   )}`;
 }
-//================= Btn Events========================
+
 const btnEvents = () => {
   playvBtnEl.addEventListener("click", togglePlay);
   nextvBtnEl.addEventListener("click", () => changeMusic(1));
   prevBtnEl.addEventListener("click", () => changeMusic(-1));
-  //========= Progressbar===========================
+
   music.addEventListener("ended", () => changeMusic(1));
   music.addEventListener("timeupdate", updateProgressBar);
   playerProgressEl.addEventListener("click", setProgressBar);
 };
-//================= Btn Events========================
+
 document.addEventListener("DOMContentLoaded", btnEvents);
-//============ Calling Load Music
+
 loadMusic(songs[musicIndex]);
